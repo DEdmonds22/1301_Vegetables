7b25dee84bdc109f6a5823282b9f49292b51d8a0
DEdmonds22 <ddedmonds22@gmail.com>
2023-08-18 20:44:06 -0400

    created Models directory, within it created a Vegetables file to hold veggie data

---
+++ Models/Vegetables.js
@@ -0,0 +1,29 @@
+const veggies = [
+    {
+        name: "beans",
+        color: "black",
+        readyToEat: false
+    },
+    {
+        name: "corn",
+        color: "yellow",
+        readyToEat: true
+    },
+    {
+        name: "strawberries",
+        color: "red",
+        readyToEat: true
+    },
+    {
+        name: "potato",
+        color: "brown",
+        readyToEat: true
+    },
+    {
+        name: "celery",
+        color: "green",
+        readyToEat: true
+    },
+];
+
+module.exports = veggies;
