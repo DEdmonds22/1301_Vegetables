cfe5a5eef3cdcc6c93660df85635e4a5f376bb8d
DEdmonds22 <ddedmonds22@gmail.com>
2023-08-18 21:28:11 -0400

    route to vegetables is completed

---
+++ Views/Index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Index = ({veggies}) => {
+    return (
+        <>
+            <h1>List of Veggies!</h1>
+            <ul>
+                {veggies.map((veggie, i) => {
+                    return (
+                        <li>
+                            The <a href={`/vegetables/${i}`}>{veggie.name}</a> is  {veggie.color} and it's {veggie.readyToEat ? "ready to eat" : "not ready to eat"}.
+                        </li>
+                    )
+                })}
+            </ul>
+        </>
+    )
+}
+
+module.exports = Index;
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const veggies = require("./Models/Vegetables");
+
+app.set('view engine', "jsx");
+app.engine('jsx', require('express-react-views').createEngine());
+
+// index; all veggies
+app.get('/vegetables', (req, res) => {
+    res.render('Index', {veggies: veggies});
+});
+
+// new; has a form to submit a new veggie
+app.get('/vegetables/new', (req, res) => {
+    res.render();
+});
+
+// show; shows each individual veggie that's clicked
+app.get('/vegetables/:index', (req, res) => {
+    res.render();
+});
+
+// handles forms request info
+app.post('/vegetables', (req, res) => {
+    console.log("posted")
+})
+
+app.listen('3000', () => {
+    console.log("Listening");
+});
